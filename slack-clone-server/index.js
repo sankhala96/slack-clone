e90b2e4d4e2c2213279ab3b8a006c7bca718ddad
sankhala <sankhalayash96@gmail.com>
2018-04-12 01:46:31 +0530

    setting up GraphQL server with node js

---
+++ slack-clone-server/index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
+import { makeExecutableSchema } from 'graphql-tools'
+
+import typeDefs from './schema'
+import resolvers from './resolver'
+
+export const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+});
+
+const app = express();
+
+const graphqlEndpoint = '/graphql';
+
+
+app.use(graphqlEndpoint,
+    bodyParser.json(),
+    graphqlExpress({ schema}));
+
+app.use('/graphiql',
+    graphiqlExpress({ endpointURL: graphqlEndpoint}));
+
+app.listen(8080, () => {
+    console.log('running on 8080')
+});
